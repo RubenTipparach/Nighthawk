b8f9e3ecca15a844a2a6ee9f89d4e8e8c52873c9
RubenTipparach <santipab.stephane@gmail.com>
2019-01-27 22:18:20 -0600

    no message

---
+++ Node-Test-API/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var express = require('express'),
+    bodyParser = require('body-parser');
+
+var cors = require('cors');
+
+var colors = require('colors');
+var strFormat = require('string-format');
+
+var logger = require('./logger.js');
+
+// config stuff
+var yaml = require('js-yaml');
+var fs = require('fs');
+var fileUpload = require('express-fileupload');
+const sqlite3 = require('sqlite3').verbose();
+
+var config = null;
+try {
+    config = yaml.safeLoad(fs.readFileSync('config.yaml', 'utf8'));
+    logger.debug(config);
+} catch (e) {
+    logger.error(e);
+}
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+
+app.listen(config.network.port, () => {
+    logger.info(`Map started on port ${config.network.port.toString().green.bold}`);
+});
+
+let db = new sqlite3.Database('./db/default.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the default db database.');
+});
+
+db.close();
+
+app.get('/', (req,res) => {
+	res.send('Node API Server is online');
+});
+
