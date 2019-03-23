ecd429a7f6a3f473aaaa8c48874f6f0a1c0db932
sirdude103 <sirdude103@gmail.com>
2019-03-23 14:36:50 -0500

    Adjusted numkey script to spawn in front of controller

--- Assets/UIScripts/MenuControl.cs
+++ Assets/UIScripts/MenuControl.cs
@@ -6,7 +6,7 @@ using BaroqueUI;
 public class MenuControl : MonoBehaviour
 {
 
-    public Dialog numpadIP;
+    public KeyboardClicker numpadIP;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +18,17 @@ public class MenuControl : MonoBehaviour
     // Update is called once per frame
     void OnTriggerDown(Controller controller)
     {
-        var popup = numpadIP.MakePopup(controller, gameObject);
+        //Dialog d;
+        //d.MakePopup(null, null);
+
+        // determine direction
+        Vector3 head_forward = controller.position - Baroque.GetHeadTransform().position;
+        Vector3 fw = controller.forward + head_forward.normalized;
+        fw.y = 0; 
+        Vector3 handposition = controller.position + (0.05f * fw);
+
+        // spawn numpad
+        var popup = Instantiate( numpadIP, handposition, Quaternion.LookRotation(fw));//.MakePopup(controller, gameObject);
         if (popup == null)
             return;
 
