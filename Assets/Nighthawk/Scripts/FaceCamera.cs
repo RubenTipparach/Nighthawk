2b17a38dcf95b35a276e052cbd0d1d04ee8d6a77
Ruben <santipab.stephane@gmail.com>
2019-12-16 00:52:23 -0600

    cam  look at fix

--- Assets/Nighthawk/Scripts/FaceCamera.cs
+++ Assets/Nighthawk/Scripts/FaceCamera.cs
@@ -14,9 +14,12 @@ public class FaceCamera : MonoBehaviour
 
     void Update()
     {
-        Vector3 v = cameraToLookAt.transform.position - transform.position;
-        v.x = v.z = 0.0f;
-        transform.LookAt(cameraToLookAt.transform.position - v);
-        transform.Rotate(0, 180, 0);
+        if (cameraToLookAt != null)
+        {
+            Vector3 v = cameraToLookAt.transform.position - transform.position;
+            v.x = v.z = 0.0f;
+            transform.LookAt(cameraToLookAt.transform.position - v);
+            transform.Rotate(0, 180, 0);
+        }
     }
 }
